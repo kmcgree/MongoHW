2653ce3273b27bd15762a5ede52ab481aecdcb0f
Kane <kmcgree14@gmail.com>
2018-11-10 12:53:17 -0600

    ASDf

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 // Routes
 
 // A GET route for scraping the echoJS website
